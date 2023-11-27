dfadc3bb98d008f7a64a1175031551a92380936a
DmytroArkhypov <dmiarkh@gmail.com>
2023-11-27 19:06:23 +0200

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import logo from "./assets/images/logo.svg";
+import bgImage from "./assets/images/bg.jpg";
+
 export default function App() {
-  return <div></div>
+  return (
+    <div
+      className="h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
+      <img src={logo} alt="anvient logo" className="mx-auto w-44 pt-16" />
+      <div className="flex flex-col items-center justify-center gap-5">
+        <span className="mt-72 font-bold tracking-widest text-mainColor">
+          COMING SOON
+        </span>
+        <h1 className="max-w-4xl text-center text-7xl font-bold leading-snug text-white">
+          We create products you all wanted so much
+        </h1>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: '#2FE48D',
+      },
+    },
   },
   plugins: [],
 }
